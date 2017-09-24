bd826b046242a6c11d939d3a3b9ecc0caf807d6d
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-09-24 17:59:32 +0800

    set state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,8 +4,18 @@ import React, { Component } from 'react'
 // Class : Define a new class called Search bar and give it access to all of the functionality that react component has.
 // It gives our search bar a bunch of functionality from the React.Component class
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: ""};
+    }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return(
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
