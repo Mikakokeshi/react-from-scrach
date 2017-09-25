73d8328cd37a2c447a7ac0cb235e2f37c0934d6d
Mikako Iijima <mikakokeshi00124@gmail.com>
2017-09-25 19:18:26 +0800

    props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDmYVX6lavWIa-MBVJF73TtujpENKZckmw';
 
-// Create a new component. This component should produce
-// some HTML
+class App extends Component{
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return <div>
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+  });
+}
+
+render() {
+  return (
+  <div>
     <SearchBar />
-  </div>;
+    <VideoList videos={this.state.videos}/>
+  </div>
+    );
+  }
 }
 
 // Take this component's generate HTML and put it
